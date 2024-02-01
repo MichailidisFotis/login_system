434228f227cfa35c62bb328b11c43e9de88b6de4
MichailidisFotis <103933469+MichailidisFotis@users.noreply.github.com>
2024-02-02 01:11:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -33,11 +33,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/users' ,usersRouter)
 
-app.get("/" , async (req ,  res)=>{
 
-	
-res.sendFile(__dirname+'/index.html')
-})
 
 app.get('/user_page' , requireLogin , async(req , res)=>{
   res.sendFile(__dirname+'/user_page.html')
